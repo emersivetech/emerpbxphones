41ff4ebf11d3cbbfc4b33c5c93026fd9f64b92e8
Jordan McDonald <jordan@j14.co>
2016-02-25 15:03:30 +1300

    Adding Panasonic package

---
+++ endpoint/panasonic/KX-HDV2xx/phone.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Panasonic KX-HDV2xx Provisioning System
+ *
+ * @author Jordan
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_panasonic_KX-HDV2xx_phone extends endpoint_panasonic_base {
+
+	public $family_line = 'KX-HDV2xx';
+	
+}
