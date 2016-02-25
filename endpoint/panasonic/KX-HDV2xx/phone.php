b5df30a30f86bad57024412406f1793fabf8c5e7
Jordan McDonald <jordan@j14.co>
2016-02-25 15:26:48 +1300

    fixed phone.php

--- endpoint/panasonic/KX-HDV2xx/phone.php
+++ endpoint/panasonic/KX-HDV2xx/phone.php
@@ -6,7 +6,7 @@
  * @license MPL / GPLv2 / LGPL
  * @package Provisioner
  */
-class endpoint_panasonic_KX-HDV2xx_phone extends endpoint_panasonic_base {
+class endpoint_panasonic_KXHDV2xx_phone extends endpoint_panasonic_base {
 
 	public $family_line = 'KX-HDV2xx';
 	
