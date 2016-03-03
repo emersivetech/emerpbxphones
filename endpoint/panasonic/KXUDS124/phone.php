13a693b009f649263aeb46dfada510b1021b0554
Jordan McDonald <j_14@me.com>
2016-03-04 11:00:41 +1300

    KX-UDS124 entry

---
+++ endpoint/panasonic/KXUDS124/phone.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Panasonic KX-HDV2xx Provisioning System
+ *
+ * @author Jordan
+ * @license MPL / GPLv2 / LGPL
+ * @package Provisioner
+ */
+class endpoint_panasonic_KXUDS124_phone extends endpoint_panasonic_base {
+
+	public $family_line = 'KXUDS124';
+
+}
